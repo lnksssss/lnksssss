f33ba23253a0f65baf12f013743d272768a1b4fe
hoon123456 <139747134+hoon123456@users.noreply.github.com>
2023-07-17 16:58:50 +0900

    Delete diex01 directory

--- diex01/src/main/java/diex01/B.java
+++
@@ -1,5 +0,0 @@
-package diex01;
-
-public class B {
-	String bfield1="b ÇÊµå1";
-}
--- diex01/src/main/java/diex01/School.java
+++
@@ -1,6 +0,0 @@
-package diex01;
-
-public class School {
-	public String name = "이젠학교";
-	public String addr = "백제대로 572";
-}
