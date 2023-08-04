bc5c2245a175bdad9d7e765d484e0ce96ea306de
lnksssss <130124775+lnksssss@users.noreply.github.com>
2023-08-04 12:29:43 +0900

    Delete StepStyle/stepstyle directory

--- StepStyle/stepstyle/src/main/webapp/resources/js/shoesDetaPage.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $(".size-table").click(function () {
-    $(".modal-wrap").show();
-  });
-	$(".modal-close").click(function () {
-    $(".modal-wrap").show();
-  });
-});
